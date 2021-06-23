a0648f1d2f0256a7be53a437b492e0cca9f1662d
Harshad Paradkar <harshad800@gmail.com>
2021-06-23 15:53:17 -0400

    About and Contacts page update
    Edited the template data to match the Movie Listings App.

---
+++ MovieListingsApp/Models/AppInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace MovieListingsApp.Models
+{
+    public class AppInfo
+    {
+        public static string Name => "Movie Listings App";
+        public static string Description => "An Asp.Net Razor MVC 5 website that displays movie listings. " +
+            "Listing page shows a list of movies, including a distinct thumbnail image.";
+        public static string Version => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+    }
+}
